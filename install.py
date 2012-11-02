00022b59255fb2b851aba9bd2f52d3ea2598b1ae
Ethan Gutmann <gutmann@ucar.edu>
2012-11-02 11:22:58 -0600

    added help message to install.py

--- install.py
+++ install.py
@@ -13,11 +13,26 @@ def install(install_dir,copyout,diff):
                 shutil.copy2(install_dir+curfile,curfile)
             else:
                 shutil.copy2(curfile,install_dir)
-    
         
+def printhelp():
+    print("""
+    Usage: install.py [-d] [-r] [-h]
+        -r = reverse copy from install dir to local dir
+        -d = diff install dir files with local files
+        -h = print this help message
+        (not all options can be passed as arguments instead e.g. install.py d )
+        """)
+
 if __name__ == '__main__':
-    opts,args=getopt.getopt(sys.argv[1:],"rd")
-    
+    opts,args=getopt.getopt(sys.argv[1:],"rdh")
+    if "h" in args:
+        printhelp()
+        sys.exit(0)
+    for opt in opts:
+        if opt[0]=="-h":
+            printhelp()
+            sys.exit(0)
+     
     host=os.uname()[1].split(".")[0]
     if re.match("mirage",host):
         host="mirage"
@@ -26,7 +41,7 @@ if __name__ == '__main__':
                       hydrolab="/home/gutmann/bin/python/",
                       nomad="/Users/gutmann/Dropbox/bin/python/statdown/",
                       pakaldi="/Users/gutmann/Dropbox/bin/python/statdown/")
-    # print(host,install_dirs[host])
+    
     copyout=False
     diff=False
     if "r" in args:
