ad3b51d81b69e7cbcef24a7d9a3fc37eb1209e10
Ethan Gutmann <gutmann@ucar.edu>
2016-05-12 16:55:06 -0600

    updates to erai helper function to add more output variables
    added swdown, lwdown, and tskin.  Also fixed a few bugs

--- helpers/erai/convert.py
+++ helpers/erai/convert.py
@@ -4,44 +4,76 @@ from bunch import Bunch
 R=8.3144621 # J/mol/K
 cp=29.19 # J/mol/K   =1.012 J/g/K
 g=9.81 # m/s^2
+global last_longwave
+last_longwave = None
+global last_shortwave
+last_shortwave = None
 
 # icar_atm_var=["u","v","gph","t","qv","ln_p_sfc","cloud","ice","sigma"]
 def convert_atm(data):
     output_data=Bunch()
-    output_data.u=data.u[::-1,::-1,:]                        # m/s
-    output_data.v=data.v[::-1,::-1,:]                        # m/s
-    output_data.hgt=data.gph[::-1,:]/g                       # (m^2/s^2) / (m/s^2) = m
+    output_data.u   = data.u[np.newaxis, ::-1,::-1,:]           # m/s
+    output_data.v   = data.v[np.newaxis, ::-1,::-1,:]           # m/s
+    output_data.hgt = data.gph[::-1,:]/g                        # (m^2/s^2) / (m/s^2) = m
 
     # calculate pressure in Pa from ln(sfc_press) and hybrid sigma coordinates
-    output_data.p=np.zeros((data.u.shape))
-    ps=np.exp(data.ln_p_sfc[::-1,:])                         # Pa
+    output_data.p = np.zeros((output_data.u.shape))
+    ps = np.exp(data.ln_p_sfc[::-1,:])                          # Pa
     for i in range(len(data.sigma_a)):
         # see http://rda.ucar.edu/datasets/ds627.0/docs/Eta_coordinate/
         # notes on http://aaron.boone.free.fr/aspdoc/node7.html might help...
-        output_data.p[len(data.sigma_a)-i-1,:,:]=(data.sigma_a[i]*data.P0+data.sigma_b[i]*ps)
+        output_data.p[0,len(data.sigma_a)-i-1,:,:]=(data.sigma_a[i]*data.P0+data.sigma_b[i]*ps)
 
     psl=ps/((1 - 2.25577E-5*output_data.hgt)**5.25588)
     output_data.z=np.zeros(output_data.p.shape)
-    for i in range(output_data.p.shape[0]):
-        output_data.z[i,...]=((output_data.p[i,...]/psl)**(1/5.25588)-1) / (-2.25577E-5)
+    for i in range(output_data.p.shape[1]):
+        output_data.z[0,i,...]=((output_data.p[0,i,...]/psl)**(1/5.25588)-1) / (-2.25577E-5)
 
     pii=(100000.0/output_data.p)**(R/cp)
-    output_data.t=data.t[::-1,::-1,:]*pii                    #K (convertred to potential temperature)
+    output_data.t=data.t[np.newaxis,::-1,::-1,:]*pii                    # K (converted to potential temperature)
     
-    output_data.qv=data.qv[::-1,::-1,:]                      # kg/kg
-    output_data.cloud=data.cloud[::-1,::-1,:]                # kg/kg
-    output_data.ice=data.ice[::-1,::-1,:]                    # kg/kg
+    output_data.qv    = data.qv[np.newaxis,::-1,::-1,:]                 # kg/kg
+    output_data.cloud = data.cloud[np.newaxis,::-1,::-1,:]              # kg/kg
+    output_data.ice   = data.ice[np.newaxis,::-1,::-1,:]                # kg/kg
     
     return output_data
 
 # icar_sfc_var=["sensible_heat","latent_heat","hgt_98","PBL_height"]
 def convert_sfc(data):
+    global last_longwave
+    global last_shortwave
     dt= -3.*60.*60.
     output_data=Bunch()
-    output_data.sensible_heat=data.sensible_heat[::-1,:]/dt  # W/m^2
-    output_data.latent_heat=data.latent_heat[::-1,:]/dt      # W/m^2
-    output_data.sfc_hgt=data.hgt_98[::-1,:]/g                # (m^2/s^2) / (m/s^2) = m
-    output_data.PBL_height=data.PBL_height[::-1,:]           # m
+    output_data.sensible_heat   = data.sensible_heat[np.newaxis,::-1,:]/dt  # W/m^2
+    output_data.latent_heat     = data.latent_heat[np.newaxis,::-1,:]/dt    # W/m^2
+    output_data.sfc_hgt         = data.hgt_98[::-1,:]/g                     # (m^2/s^2) / (m/s^2) = m
+    output_data.PBL_height      = data.PBL_height[np.newaxis,::-1,:]        # m
+    output_data.tskin           = data.tskin[np.newaxis,::-1,:]             # K
+    output_data.sw              = data.sw[np.newaxis,::-1,:] / dt   # convert from Joules to W /m^2
+    output_data.lw              = data.lw[np.newaxis,::-1,:] / dt   # convert from Joules to W /m^2 
+    
+    # this is now handled in io so it can just use the last value in the file, much simple
+    #  ... though in some ways what is below is better as it integrates over a longer time period
+    # if last_longwave==None:
+    #     last_shortwave = np.zeros(output_data.sw.shape) + output_data.sw
+    #     last_longwave = np.zeros(output_data.lw.shape) + output_data.lw
+    #     # initial values for the day were only integrated over a 3hr period
+    #     output_data.sw*=2
+    #     output_data.lw*=2
+    # else:
+    #     if output_data.lw.max() > last_longwave.max():
+    #         temp = np.zeros(output_data.lw.shape) + output_data.lw
+    #         output_data.lw -= last_longwave
+    #         last_longwave = temp
+    #         temp = np.zeros(output_data.sw.shape) + output_data.sw
+    #         output_data.sw -= last_shortwave
+    #         last_shortwave = temp
+    #     else:
+    #         last_longwave[:] = output_data.lw
+    #         last_shortwave[:] = output_data.sw
+    #         # initial values for the day were only integrated over a 3hr period
+    #         output_data.sw*=2
+    #         output_data.lw*=2
     return output_data
 
 def era2icar(data):
