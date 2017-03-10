2ff1c189a4dc45278e864d51a644dac65f7879d3
Damian Rouson <damian@sourceryinstitute.org>
2017-03-10 10:30:12 -0800

    In string.f90, Specify real kinds & make elemental

--- utilities/string.f90
+++ utilities/string.f90
@@ -8,6 +8,8 @@
 !!------------------------------------------------------------
 module string
 
+    use iso_fortran_env, only : real32, real64
+
     implicit none
     interface str
         module procedure str_d
@@ -17,33 +19,31 @@ module string
     
     integer,parameter::MAXSTRINGLENGTH=100
 contains
-    function get_double(str_in)
+    elemental function get_double(str_in)
         implicit none
         character(len=*), intent(in) :: str_in
-        double precision :: get_double
+        real(real64) :: get_double
         read(str_in,*) get_double
     end function get_double
     
-    function get_real(str_in)
+    elemental function get_real(str_in)
         implicit none
         character(len=*), intent(in) :: str_in
-        real :: get_real
+        real(real32) :: get_real
         read(str_in,*) get_real
     end function get_real
 
-    function get_integer(str_in)
+    elemental function get_integer(str_in)
         implicit none
         character(len=*), intent(in) :: str_in
         integer :: get_integer
         read(str_in,*) get_integer
     end function get_integer
     
-    
-    
-    function str_d(value,fmt) result(output_string)
+    elemental function str_d(value,fmt) result(output_string)
         implicit none
-        double precision :: value
-        character(len=*), optional :: fmt
+        real(real64), intent(in) :: value
+        character(len=*), optional, intent(in) :: fmt
         character(len=MAXSTRINGLENGTH) :: output_string
         if (present(fmt)) then
             write(output_string,fmt) value
@@ -53,10 +53,10 @@ contains
         output_string=adjustl(output_string)
     end function str_d
 
-    function str_r(value,fmt) result(output_string)
+    elemental function str_r(value,fmt) result(output_string)
         implicit none
-        real :: value
-        character(len=*), optional :: fmt
+        real(real32), intent(in) :: value
+        character(len=*), optional, intent(in) :: fmt
         character(len=MAXSTRINGLENGTH) :: output_string
         if (present(fmt)) then
             write(output_string,fmt) value
@@ -66,10 +66,10 @@ contains
         output_string=adjustl(output_string)
     end function str_r
 
-    function str_i(value,fmt) result(output_string)
+    elemental function str_i(value,fmt) result(output_string)
         implicit none
-        integer :: value
-        character(len=*), optional :: fmt
+        integer, intent(in) :: value
+        character(len=*), optional, intent(in) :: fmt
         character(len=MAXSTRINGLENGTH) :: output_string
         if (present(fmt)) then
             write(output_string,fmt) value
@@ -78,6 +78,5 @@ contains
         endif
         output_string=adjustl(output_string)
     end function str_i
-
 
 end module string
