eb2bb1ee52dc71b723597db9f694051552f90930
Ethan Gutmann <gutmann@ucar.edu>
2017-12-10 00:13:55 -0700

    added test for initializing domain and writing output (no data significant read in yet)

--- src/io/output_obj.f90
+++ src/io/output_obj.f90
@@ -76,19 +76,21 @@ contains
         character(len=49)       :: date_format
         character(len=5)        :: UTCoffset
         character(len=64)       :: err
+        integer                 :: ncid
 
+        ncid = this%ncfile_id
 
         err="Creating global attributes"
         call check( nf90_put_att(ncid,NF90_GLOBAL,"Conventions","CF-1.6"), trim(err))
         call check( nf90_put_att(ncid,NF90_GLOBAL,"title","Intermediate Complexity Atmospheric Research Model output"), trim(err))
         call check( nf90_put_att(ncid,NF90_GLOBAL,"institution","National Center for Atmospheric Research"), trim(err))
-        call check( nf90_put_att(ncid,NF90_GLOBAL,"source","Intermediate Complexity Atmospheric Model version:"//trim(options%version)), trim(err))
+        ! call check( nf90_put_att(ncid,NF90_GLOBAL,"source","Intermediate Complexity Atmospheric Model version:"//trim(options%parameters%version)), trim(err))
         call check( nf90_put_att(ncid,NF90_GLOBAL,"history","Created:"//todays_date_time//UTCoffset), trim(err))
         call check( nf90_put_att(ncid,NF90_GLOBAL,"references", &
                     "Gutmann et al. 2016: The Intermediate Complexity Atmospheric Model (ICAR). J.Hydrometeor. doi:10.1175/JHM-D-15-0155.1, 2016."), trim(err))
         ! call check( nf90_put_att(ncid,NF90_GLOBAL,"comment",trim(options%parameters%comment)), trim(err))
         call check( nf90_put_att(ncid,NF90_GLOBAL,"contact","Ethan Gutmann : gutmann@ucar.edu"), trim(err))
-        call check( nf90_put_att(ncid,NF90_GLOBAL,"git",VERSION), trim(err))
+        ! call check( nf90_put_att(ncid,NF90_GLOBAL,"git",VERSION), trim(err))
 
         ! general physics options
         ! call check( nf90_put_att(ncid,NF90_GLOBAL,"microphysics",  options%physics%microphysics),   trim(err))
