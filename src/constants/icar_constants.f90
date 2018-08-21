c52f31db073d72fac69ac4395bda17bd8504f76e
Ethan Gutmann <gutmann@ucar.edu>
2018-08-21 11:10:23 -0600

    Prep work for 1.0 tag

--- src/constants/icar_constants.f90
+++ src/constants/icar_constants.f90
@@ -1,7 +1,7 @@
 module icar_constants
 
     implicit none
-    character(len=5) :: kVERSION_STRING = "0.9.5"
+    character(len=5) :: kVERSION_STRING = "1.0"
 
 ! ------------------------------------------------
 ! Model constants (string lengths)
