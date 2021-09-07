8643d0fd48b4938509d42e8f50893809a6b2c10f
Soren Rasmussen <s.c.rasmussen@gmail.com>
2021-09-07 13:09:44 -0700

    Updating the actions to only build one possible version of ICAR but not to run ICAR. Will run ICAR after test data generation is working. Updating status badge from TravisCI to Github Actions

--- .github/scripts/icar_install_utils.sh
+++ .github/scripts/icar_install_utils.sh
@@ -3,10 +3,10 @@
 set -e
 set -x
 
-export FC=gfortran-9
+export FC=gfortran-11
 # see link for size of runner
 # https://docs.github.com/en/actions/using-github-hosted-runners/about-github-hosted-runners#supported-runners-and-hardware-resources
-export JN=-j2
+export JN=-j
 
 if [ -z "$WORKDIR" ]; then
     export WORKDIR=$HOME/workdir
@@ -27,18 +27,19 @@ function install_szip {
     ./configure --prefix=$INSTALLDIR &> config.log
     make &> make.log
     make install ${JN}
-    export CPPFLAGS="$CPPFLAGS -I${INSTALLDIR}/include"
-    export LDFLAGS="$LDFLAGS -L${INSTALLDIR}/lib"
 }
 
 function install_hdf5 {
     echo install_hdf5
     cd $WORKDIR
-    wget --no-check-certificate -q https://support.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.10.5.tar.gz
-    tar -xzf hdf5-1.10.5.tar.gz
-    cd hdf5-1.10.5
-    ./configure --prefix=$INSTALLDIR &> config.log
-    (make | awk 'NR%100 == 0')
+    wget --no-check-certificate -q https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.7/src/hdf5-1.10.7.tar.gz
+    tar -xzf hdf5-1.10.7.tar.gz
+    cd hdf5-1.10.7
+    # FCFLAGS="-DH5_USE_110_API" ./configure --prefix=$INSTALLDIR &> config.log
+    ./configure --prefix=$INSTALLDIR #&> config.log
+    make
+    # CFLAGS=-DH5_USE_110_API make
+    # (CFLAGS=-DH5_USE_110_API make | awk 'NR%100 == 0')
     make install ${JN}
     export LIBDIR=${INSTALLDIR}/lib
 }
@@ -72,6 +73,9 @@ function icar_dependencies {
     sudo apt-get update
     sudo apt-get install libcurl4-gnutls-dev
 
+    export CPPFLAGS="$CPPFLAGS -I${INSTALLDIR}/include"
+    export LDFLAGS="$LDFLAGS -L${INSTALLDIR}/lib"
+
     # Install szip (used by hdf5)
     install_szip
     # Install HDF5
@@ -91,19 +95,19 @@ function icar_install {
 
     export NETCDF=${INSTALLDIR}
     export FFTW=/usr
+    export FFTW=/Users/soren/local/brew/3.1.7/Cellar/fftw/3.3.9_1
 
     # CAF_MODE=single tells it to compile with gfortran -fcoarray=single
-    make -C src clean; make -C src -j4 icar CAF_MODE=single
     # test serial build
     make -C src clean; make -C src     CAF_MODE=single MODE=debugslow
-    # test parallel builds with different compile settings
-    make -C src clean; make -C src -j4 CAF_MODE=single MODE=debugslow
-    make -C src clean; make -C src -j4 CAF_MODE=single MODE=debug
-    make -C src clean; make -C src -j4 CAF_MODE=single MODE=debugompslow
-    make -C src clean; make -C src -j4 CAF_MODE=single MODE=debugomp
-    make -C src clean; make -C src -j4 CAF_MODE=single MODE=profile
-    # make -C src clean; make -C src -j4 CAF_MODE=single MODE=fast
-    make -C src clean; make -C src -j4 CAF_MODE=single
+    # # test parallel builds with different compile settings
+    # make -C src clean; make -C src -j4 CAF_MODE=single MODE=debugslow
+    # make -C src clean; make -C src -j4 CAF_MODE=single MODE=debug
+    # make -C src clean; make -C src -j4 CAF_MODE=single MODE=debugompslow
+    # make -C src clean; make -C src -j4 CAF_MODE=single MODE=debugomp
+    # make -C src clean; make -C src -j4 CAF_MODE=single MODE=profile
+    # # make -C src clean; make -C src -j4 CAF_MODE=single MODE=fast
+    # make -C src clean; make -C src -j4 CAF_MODE=single
     # make -C src -j4 CAF_MODE=single test
     echo "icar install succeeded"
 
@@ -136,7 +140,6 @@ function icar_after_failure {
   echo icar_after_failure
   echo "icar build failed"
 }
-
 
 for func in "$@"
 do
