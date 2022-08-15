76a75b46223b233ffa4aff0308df149a31cd78ab
Ethan Gutmann <gutmann@ucar.edu>
2022-08-15 09:31:57 -0600

    Update icar_constants.f90
    adding number for combination of albedo and lake_depth variables

--- src/constants/icar_constants.f90
+++ src/constants/icar_constants.f90
@@ -253,7 +253,7 @@ module icar_constants
                                                             171, 172, 173, 174, 175, 176, 177, 178, 179, 180,  &
                                                             181, 182, 183, 184, 185, 186, 187, 188, 189, 190,  &
                                                             191, 192, 193, 194, 195, 196, 197, 198, 199, 200,  &
-                                                            201)
+                                                            201, 202)
 
     integer, parameter :: kINTEGER_BITS     = storage_size(kINTEGER_BITS)
     integer, parameter :: kMAX_STORAGE_VARS = storage_size(kVARS) / kINTEGER_BITS
