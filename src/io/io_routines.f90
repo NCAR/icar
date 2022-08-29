a089522779543eb0f9b2755f8be8aca533688e36
Ethan Gutmann <gutmann@ucar.edu>
2022-08-29 13:47:28 -0600

    add double precision variables to restart capability

--- src/io/io_routines.f90
+++ src/io/io_routines.f90
@@ -28,7 +28,8 @@ module io_routines
     !! Generic interface to the netcdf read routines
     !!------------------------------------------------------------
     interface io_read
-        module procedure io_read6d, io_read5d, io_read4d, io_read3d, io_read2d, io_read1d, io_read2di, io_read1dd, io_read_scalar_d, io_read0d, io_read0di
+        module procedure io_read6d, io_read5d, io_read4d, io_read3d, io_read2d, io_read1d,  &
+            io_read2dd, io_read2di, io_read1dd, io_read_scalar_d, io_read0d, io_read0di
     end interface
 
     !>------------------------------------------------------------
@@ -505,6 +506,65 @@ contains
         call check( nf90_close(ncid),filename)
 
     end subroutine io_read2d
+
+    subroutine io_read2dd(filename,varname,data_in,extradim)
+        implicit none
+        ! This is the name of the data_in file and variable we will read.
+        character(len=*), intent(in) :: filename, varname
+        double precision,intent(out),allocatable :: data_in(:,:)
+        integer, intent(in),optional :: extradim
+        integer, dimension(io_maxDims)  :: diminfo ! will hold dimension lengths
+        integer, dimension(io_maxDims)  :: dimstart
+        ! This will be the netCDF ID for the file and data_in variable.
+        integer :: ncid, varid,i, err
+        real :: scale, offset
+
+        if (present(extradim)) then
+            dimstart=extradim
+            dimstart(1:2)=1
+        else
+            dimstart=1
+        endif
+
+        diminfo = 1
+        ! Read the dimension lengths
+        call io_getdims(filename,varname,diminfo)
+
+        if (allocated(data_in)) deallocate(data_in)
+        allocate(data_in(diminfo(2),diminfo(3)))
+
+        ! Open the file. NF90_NOWRITE tells netCDF we want read-only access to
+        ! the file.
+        call check(nf90_open(filename, NF90_NOWRITE, ncid),filename)
+        ! Get the varid of the data_in variable, based on its name.
+        call check(nf90_inq_varid(ncid, varname, varid),trim(filename)//":"//trim(varname))
+        err = nf90_get_att(ncid,varid,'scale_factor', scale)
+        if (err/=0) scale=1
+        err = nf90_get_att(ncid,varid,'add_offset', offset)
+        if (err/=0) offset=0
+
+
+
+        ! Read the data_in. skip the slowest varying indices if there are more than 3 dimensions (typically this will be time)
+        if (diminfo(1)>2) then
+            diminfo(4:diminfo(1)+1)=1 ! set count for extra dims to 1
+            call check(nf90_get_var(ncid, varid, data_in,&
+                                    dimstart(1:diminfo(1)), &               ! start  = 1 or extradim
+                                    [ (diminfo(i+1), i=1,diminfo(1)) ],&    ! count=n or 1 created through an implied do loop
+                                    [ (1,            i=1,diminfo(1)) ] ), & ! for all dims, stride = 1      " implied do loop
+                                    trim(filename)//":"//trim(varname)) !pass varname to check so it can give us more info
+        elseif (diminfo(1)==1) then
+            call check(nf90_get_var(ncid, varid, data_in(:,1)),trim(filename)//":"//trim(varname))
+        else
+            call check(nf90_get_var(ncid, varid, data_in),trim(filename)//":"//trim(varname))
+        endif
+        data_in = data_in * scale + offset
+
+        ! Close the file, freeing all resources.
+        call check( nf90_close(ncid),filename)
+
+    end subroutine io_read2dd
+
 
     !>------------------------------------------------------------
     !! Same as io_read2d but for integer data
