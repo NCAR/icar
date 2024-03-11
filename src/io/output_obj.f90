febd23792a1b2ad018f6c1411b82ef67ae3da52b
Soren Rasmussen <s.c.rasmussen@gmail.com>
2024-03-11 14:50:33 -0600

    bugfix for negative precip. Negative precip occurs if output file is being overwritten. Fix always creates a new file at the start of the day.

--- src/io/output_obj.f90
+++ src/io/output_obj.f90
@@ -47,10 +47,16 @@ contains
 
         if (.not.this%is_initialized) call this%init()
 
-        ! open file
+        err = 0
         this%filename = filename
-        err = nf90_open(filename, NF90_WRITE, this%ncfile_id)
-        if (err /= NF90_NOERR) then
+        ! if hour of the day is 0 file will never be opened but clobbered if
+        ! it exists, otherwise open file
+        if (time%hour /= 0) then
+           err = nf90_open(filename, NF90_WRITE, this%ncfile_id)
+        end if
+
+        ! create new file if first output of the day or error opening
+        if (err /= NF90_NOERR .or. (time%hour == 0)) then
             call check( nf90_create(filename, NF90_CLOBBER, this%ncfile_id), "Opening:"//trim(filename))
             this%creating=.True.
         else
